992fde299f49f9bfcfc0b69e27549c3d1f8a5f98
FaryalFaisal29 <164797311+FaryalFaisal29@users.noreply.github.com>
2024-03-26 01:18:07 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+// (5>2) && (2>0) is true
+// (5<0) || (2>0) is true
+// !(5>0) is false
+let a = 5;
+let b = 2;
+let logicalAnd = (a > 0) && (b > 0);
+console.log(logicalAnd);
+let logicalOr = (a < 0) || (b > 0);
+console.log(logicalOr);
+let logicalNot = !(a > 0);
+console.log(logicalNot);
+export {};
